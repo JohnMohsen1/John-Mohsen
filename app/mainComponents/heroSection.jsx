e0cf2dec7be237e1c3e7ef797173559282120a6b
JohnMohsen1 <143970320+JohnMohsen1@users.noreply.github.com>
2025-07-05 12:14:54 +0300

    heroSection.jsx

--- app/mainComponents/heroSection.jsx
+++ app/mainComponents/heroSection.jsx
@@ -1,11 +1,5 @@
-/* "use client"
 
-import lottie from 'lottie-web';
-import { defineElement } from 'lord-icon-element';
-
-// define "lord-icon" custom element with default properties
-defineElement(lottie.loadAnimation); */
-
+import HeroHeaderCircle from "./heroCircleIcons"
 
 const HeroSection = () => {
 
@@ -16,12 +10,11 @@ const HeroSection = () => {
           <h1>Hi, I'm John Mohsen</h1>
           <h2 style={{ fontWeight: 300 }}>Graphic Designer</h2>
           <p>
-            I am a passionate graphic designer with strong expertise in Adobe Creative Suite,
-            including Photoshop, Illustrator, and InDesign. I specialize in creating visually compelling designs
-            that communicate clearly and resonate with target audiences across digital and print media. As a professional
-            AI user, I leverage cutting-edge tools to streamline design workflows and elevate the quality of my creative output.
-            My work consistently enhances brand identity and delivers impactful visual solutions. I’m driven by the challenge of
-            turning creative ideas into polished, professional designs that inspire and engage.
+            I'm a passionate graphic designer with solid experience in Adobe Creative Suite, 
+            including Photoshop, Illustrator, and InDesign. Skilled in creating visually compelling designs
+            that communicate clearly and connect with target audiences across digital and print media. 
+            Proficient AI user focused on streamlining workflows and enhancing creative output.
+             Proven ability to elevate brand identity through impactful visual solutions. Driven by the challenge of turning ideas into polished, professional designs that engage and inspire.
           </p>
           <button className="resume btn"> 
             <div>
@@ -36,58 +29,7 @@ const HeroSection = () => {
         <div className="homeSectionImage">
           <img src="./hero_imgs/herpPic.png" alt="" className="heroPic" />
         </div>
-
-        {/*<div className="heroHeaderCircleContainer">
-          <span className="heroHeaderCircle" id="menucircle">
-            <img src="./hero_imgs/menuWhite.png" alt="" />
-          </span>
-
-          <a href="#Home">
-            <span className="heroHeaderCircle" id="homecircle" title="Home">
-              <lord-icon
-                src="https://cdn.lordicon.com/slduhdil.json"
-                trigger="morph"
-                colors="primary:#ffffff"
-                state="morph-2"
-              ></lord-icon>
-            </span>
-          </a>
-
-          <a href="#About">
-            <span className="heroHeaderCircle" id="aboutcircle" title="About Me">
-              <lord-icon
-                src="https://cdn.lordicon.com/ncxoarcp.json"
-                trigger="hover"
-                colors="primary:#ffffff"
-              ></lord-icon>
-            </span>
-          </a>
-
-          <a href="#Skills">
-            <span className="heroHeaderCircle" id="workcircle" title="Skills">
-              <lord-icon
-                src="/hero_imgs/wired-flat-2844-magic-wand-hover-pinch.json"
-                trigger="hover"
-                colors="primary:#ffffff,secondary:#ffffff,tertiary:#ffffff"
-              ></lord-icon>
-            </span>
-          </a>
-
-          <a href="#Projects">
-            <span className="heroHeaderCircle" id="contactcircle" title="Designs">
-              <lord-icon
-                src="https://cdn.lordicon.com/oezixobx.json"
-                trigger="hover"
-                colors="primary:#ffffff"
-                state="hover"
-              ></lord-icon>
-            </span>
-          </a>
-
-          <span className="heroHeaderCircle" id="closecircle">
-            <img src="./hero_imgs/close.png" alt="" />
-          </span>
-        </div>*/}
+        <HeroHeaderCircle/>
       </section>
     </div>
   );
